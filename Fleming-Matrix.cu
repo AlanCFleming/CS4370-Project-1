56270052e76e9e9fdcfafa38da1cf314de4b7985
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-09-14 23:49:29 -0400

    redefined N as SIZE to avoid conflict with function parameter

--- Fleming-Matrix.cu
+++ Fleming-Matrix.cu
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <cuda.h>
-#define N 100 //N can not be larger than 256
+#define SIZE 100 //N can not be larger than 256
 
 
 void add_matrix_cpu(int *a, int *b, int *c, int N){

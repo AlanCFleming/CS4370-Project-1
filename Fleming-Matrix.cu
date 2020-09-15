8b359422d63bde87a7813957247212795399d118
AlanCFleming <37807852+AlanCFleming@users.noreply.github.com>
2020-09-14 23:41:24 -0400

    Added cpu based matric addition

--- Fleming-Matrix.cu
+++ Fleming-Matrix.cu
@@ -1,3 +1,14 @@
 #include <stdio.h>
 #include <cuda.h>
 #define N 100 //N can not be larger than 256
+
+
+void add_matrix_cpu(int *a, int *b, int *c, int N){
+	int i, j index;
+	for(i=0; i<N; i++){
+		for(j = 0; j<N; j++){
+			index = i*N+j;
+			c[index] = a[index] + b[index];
+		}
+	}
+}
